ca088999c20024fb4e5c9c6682050924e01121bc
Harsh-rjn21 <harsh.ranjan.cse21@itbhu.ac.in>
2026-01-02 02:36:17 +0530

    Restore local dev support in next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'http://auth-service:8000/:path*',
+      },
+      {
+        source: '/api/catalog/:path*',
+        destination: 'http://catalog-service:8000/:path*',
+      },
+      {
+        source: '/api/rental/:path*',
+        destination: 'http://rental-service:8000/:path*',
+      },
+      {
+        source: '/api/feedback/:path*',
+        destination: 'http://feedback-service:8000/:path*',
+      }
+    ];
+  },
 };
 
 export default nextConfig;
