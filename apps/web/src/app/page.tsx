308fa2c572ede08b6fc074213e27cd7d8276ef43
Harsh-rjn21 <harsh.ranjan.cse21@itbhu.ac.in>
2026-01-02 02:43:01 +0530

    testing cicd

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <Link href="/signup" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg">
-                      Start Renting
+                      Start Renting now
                     </Link>
                   </div>
                 </div>
